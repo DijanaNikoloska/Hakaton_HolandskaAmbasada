756938c9936af910f6c746d976612b41bea1f314
Dijana Nikoloska <dijana.blazovska@yahoo.com>
2025-03-01 12:58:26 +0100

    Routes done

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -4,7 +4,7 @@ function LandingPage() {
   return (
     <div className="LandingImg">
       <div className="LandingText">
-        <b>Expand</b> Your Mind, Elevate Your Future
+        <b>Expand</b> Your Mind, Elevate<b>Your Future</b>
       </div>
     </div>
   );
