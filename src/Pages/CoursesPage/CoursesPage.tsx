5b2260cab60b2e576a04a6f10ba44988aa580c26
Dijana Nikoloska <dijana.blazovska@yahoo.com>
2025-03-02 11:45:51 +0100

    updates

--- src/Pages/CoursesPage/CoursesPage.tsx
+++ src/Pages/CoursesPage/CoursesPage.tsx
@@ -1,5 +1,29 @@
+import CardList from "../Cards/Cards";
+
 function CoursesPage() {
-  return <div>Courses Page</div>;
+  return (
+    <>
+      <div className="NewSection">
+        <div>
+          <img
+            src="/images/Hero Section - Illustration.png"
+            alt="Learning"
+            className="Hero Section - Illustration"
+          />
+        </div>
+        <div className="new-section-text">
+          <h3>Unlock your learning potential</h3>
+          <p>
+            Explore a wide range of beginner-friendly courses designed to build
+            your skills and knowledge. Whether you're starting fresh or looking
+            to strengthen your foundation, our expertly crafted lessons will
+            guide you every step of the way. Start learning today!
+          </p>
+        </div>
+      </div>
+      <CardList />
+    </>
+  );
 }
 
 export default CoursesPage;
