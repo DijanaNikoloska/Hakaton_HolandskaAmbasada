edd4c015481ca08da89773b8d24a02559878af01
HEStefann <stefanhadjieftimov@gmail.com>
2025-03-02 10:34:06 +0100

    update

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,9 @@
 function LoginPage() {
-  return <div></div>;
+  return <div className="w-screen h-screen absolute top-0 left-0 flex justify-center items-center">
+    <div
+  className="flex justify-start items-center w-[945.41px] gap-[61.621620178222656px] p-[51.89189147949219px] rounded-[4.05px] bg-[#fbfbf8] border-[1.62px] border-[#024f40]"
+></div>
+  </div>;
 }
 
 export default LoginPage;
